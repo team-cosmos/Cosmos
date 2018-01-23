85ab01b8461a7744e4336d16a482a73b021beb75
Seongmin Park <seongminnpark@gmail.com>
2018-01-23 11:22:20 -0500

    Before migrating to linked lists

--- grid/grid.sol
+++ grid/grid.sol
@@ -23,6 +23,8 @@ contract Grid {
     /**
      * Assign an owner to a smart meter.
      *
+     * Has to be sent by a smart meter!!!
+     *
      * @param owner The address of the owner.
      * @return success Operation was successful.
      */
@@ -31,7 +33,110 @@ contract Grid {
 
         ownerOf[msg.sender] = user;
 
+        metersOf[user].push(msg.sender);
+
         return true;
+    }
+
+    /**
+     * Transfer ownership of a smart meter.
+     * 
+     * @param newOwner The address of the new owners.
+     * @param meter The address of the meter.
+     * @return success Operation was successful.
+     */
+    function transferMeter(address newOwner, address meter) public returns (bool success) {
+        require(newOwner != 0x0);
+        require(meter != 0x0);
+        require(ownsMeter(msg.sender, meter));
+
+        ownerOf[meter] = newOwner;
+
+        metersOf[newOwner].push(meter);
+
+        address[] meters = metersOf[msg.sender];
+
+        for (uint i=0; i < meters.length(); i++) {
+            delete();
+        }
+
+        return true;
+    }
+
+    /**
+     * Delete a smart meter.
+     * 
+     * A discussion of semantics:
+     * https://english.stackexchange.com/questions/25931/unregister-vs-deregister
+     *
+     * @param meter The address of the meter.
+     * @return success Operation was successful.
+     */
+    function deregisterMeter(address meter) public returns (bool success) {
+        require(meter != 0x0);
+        require(ownsMeter(msg.sender, meter));
+
+        ownerOf[meter] = user;
+        
+
+        return true;
+    }
+
+    /**
+     * Get owner of a meter's address.
+     *
+     * @return success Operation was successful.
+     * @return user Address of the calling meter's owner.
+     */
+    function getOwner() public returns (bool success, address user) {
+        address owner = ownerOf[msg.sender];
+
+        success = (owner == 0x0);
+
+        return (success, owner);
+    }
+
+    /**
+     * Get a list of meters that belong to the caller.
+     *
+     * @return success Operation was successful.
+     * @return meters Addresses of the calling user's meters.
+     */
+    function getMeters() public returns (bool success, address meters) {
+        address[] meters = metersOf[msg.sender];
+
+        success = (meters && meters.length > 0);
+
+        return (success, meters);
+    }
+
+    /**
+     * Check if a meter belongs to a user.
+     *
+     * @param user The address of the potential owner.
+     * @param meter The address of the meter..
+     * @return belongs Meter belongs to the user.
+     */
+    function ownsMeter(address user, address meter) public returns (bool belongs) {
+        require(meter != 0x0);
+        require(user != 0x0);
+
+        address[] meters = metersOf[user];
+
+        if (meters.length() < 1) {
+
+            return false;
+
+        } else { // Valid list of meters registered under user.
+
+            for (uint i=0; i < meters.length(); i++) {
+                if (meters[i] == meter) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
     /**
