58115459a10a7da96c6ec7a3bf36f52d9fba32de
Seongmin Park <seongminnpark@gmail.com>
2018-01-23 11:50:57 -0500

    Finish grid

--- grid/grid.sol
+++ grid/grid.sol
@@ -6,7 +6,7 @@ contract Grid {
     mapping (address => address) private ownerOf;
 
     /** Map address of users to address of their smart meters. */
-    mapping(uint => address[]) private metersOf;
+    mapping(address => address[]) private metersOf;
 
     /** Map address of users to their remaining energy balance. */
     mapping (address => uint256) private energyBalanceOf;
@@ -16,8 +16,42 @@ contract Grid {
      *
      * Constructor that sets up the grid.
      */
-    function Grid() {
+    function Grid() public {
 
+    }
+
+
+    ////////////////////////////////////////////////////////////////////
+    //////////////////////// Internal functions ////////////////////////
+    ////////////////////////////////////////////////////////////////////
+
+    /**
+     * Check if a meter belongs to a user.
+     *
+     * @param user The address of the potential owner.
+     * @param meter The address of the meter..
+     * @return belongs Meter belongs to the user.
+     */
+    function _hasMeter(address user, address meter) internal view returns (bool belongs) {
+        require(meter != 0x0);
+        require(user != 0x0);
+
+        address[] memory meters = metersOf[user];
+
+        if (meters.length < 1) {
+
+            return false;
+
+        } else { // Valid list of meters registered under user.
+
+            for (uint i=0; i < meters.length; i++) {
+                if (meters[i] == meter) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
     /**
@@ -25,7 +59,66 @@ contract Grid {
      *
      * Has to be sent by a smart meter!!!
      *
-     * @param owner The address of the owner.
+     * @param user The address of the owner.
+     * @param meter The address of the meter.
+     * @return success Operation was successful.
+     */
+    function _deregisterMeter(address user, address meter) internal view returns (bool success) {
+        require(user != 0x0);
+        require(meter != 0x0);
+        require(_hasMeter(msg.sender, meter));
+
+        address[] memory meters = metersOf[msg.sender];
+
+        // Find meter in current owner's array.
+        int meterIndex = -1;
+        for (uint i=0; i < meters.length; i++) {
+            if (meters[i] == meter) {
+                meterIndex = int(i);
+            }
+        }
+
+        // Remove from array.
+        if (meterIndex != -1) {
+            delete(meters[i]);
+            return true;
+        } else {
+            return false;
+        }  
+    }
+
+    /**
+     * Internal Energy transfer function that can be called only by this contract.
+     *
+     * @param _from The address of the sender.
+     * @param _to The address of the recipient.
+     * @param _value The amount to send in kW.
+     * @return success Operation was successful.
+     */
+    function _transferEnergy(address _from, address _to, uint256 _value) internal {
+        require(_from != 0x0);
+        require(_to != 0x0);
+        require(energyBalanceOf[_from] >= _value);
+        require(energyBalanceOf[_to] + _value >= energyBalanceOf[_to]); // Prevent overflow
+
+        uint previousBalances = energyBalanceOf[_from] + energyBalanceOf[_to];
+        energyBalanceOf[_from] -= _value;
+        energyBalanceOf[_to] += _value;
+
+        assert(energyBalanceOf[_from] + energyBalanceOf[_to] == previousBalances);
+    }
+
+
+    ////////////////////////////////////////////////////////////////////
+    ///////////////////////// Public functions /////////////////////////
+    ////////////////////////////////////////////////////////////////////
+
+    /**
+     * Assign an owner to a smart meter.
+     *
+     * Has to be sent by a smart meter!!!
+     *
+     * @param user The address of the owner.
      * @return success Operation was successful.
      */
     function registerOwner(address user) public returns (bool success) {
@@ -48,19 +141,18 @@ contract Grid {
     function transferMeter(address newOwner, address meter) public returns (bool success) {
         require(newOwner != 0x0);
         require(meter != 0x0);
-        require(ownsMeter(msg.sender, meter));
+        require(_hasMeter(msg.sender, meter));
 
-        ownerOf[meter] = newOwner;
+        if (_deregisterMeter(msg.sender, meter)) {
 
-        metersOf[newOwner].push(meter);
+            ownerOf[meter] = newOwner;
+            metersOf[newOwner].push(meter);
+            return true;
 
-        address[] meters = metersOf[msg.sender];
+        } else {
 
-        for (uint i=0; i < meters.length(); i++) {
-            delete();
+            return false;
         }
-
-        return true;
     }
 
     /**
@@ -72,14 +164,8 @@ contract Grid {
      * @param meter The address of the meter.
      * @return success Operation was successful.
      */
-    function deregisterMeter(address meter) public returns (bool success) {
-        require(meter != 0x0);
-        require(ownsMeter(msg.sender, meter));
-
-        ownerOf[meter] = user;
-        
-
-        return true;
+    function deregisterMeter(address meter) public view returns (bool success) {
+        return _deregisterMeter(msg.sender, meter);
     }
 
     /**
@@ -88,7 +174,7 @@ contract Grid {
      * @return success Operation was successful.
      * @return user Address of the calling meter's owner.
      */
-    function getOwner() public returns (bool success, address user) {
+    function getOwner() public view returns (bool success, address user) {
         address owner = ownerOf[msg.sender];
 
         success = (owner == 0x0);
@@ -102,41 +188,12 @@ contract Grid {
      * @return success Operation was successful.
      * @return meters Addresses of the calling user's meters.
      */
-    function getMeters() public returns (bool success, address meters) {
-        address[] meters = metersOf[msg.sender];
+    function getMeters() public view returns (bool success, address[] meters) {
+        meters = metersOf[msg.sender];
 
-        success = (meters && meters.length > 0);
+        success = (meters.length > 0);
 
         return (success, meters);
-    }
-
-    /**
-     * Check if a meter belongs to a user.
-     *
-     * @param user The address of the potential owner.
-     * @param meter The address of the meter..
-     * @return belongs Meter belongs to the user.
-     */
-    function ownsMeter(address user, address meter) public returns (bool belongs) {
-        require(meter != 0x0);
-        require(user != 0x0);
-
-        address[] meters = metersOf[user];
-
-        if (meters.length() < 1) {
-
-            return false;
-
-        } else { // Valid list of meters registered under user.
-
-            for (uint i=0; i < meters.length(); i++) {
-                if (meters[i] == meter) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
     }
 
     /**
@@ -151,28 +208,6 @@ contract Grid {
 
         energyBalanceOf[ownerOf[msg.sender]] += value;
         return true;
-    }
-
-    /**
-     * Internal Energy transfer function that can be called only by this contract.
-     *
-     * @param _from The address of the sender.
-     * @param _to The address of the recipient.
-     * @param _value The amount to send in kW.
-     * @return success Operation was successful.
-     */
-    function _transferEnergy(address _from, address _to, uint256 _value) internal {
-        require(_to != 0x0);
-        require(energyBalanceOf[_from] >= _value);
-        require(energyBalanceOf[_to] + _value >= energyBalanceOf[_to]); // Prevent overflow
-
-        uint previousBalances = energyBalanceOf[_from] + energyBalanceOf[_to];
-        energyBalanceOf[_from] -= _value;
-        energyBalanceOf[_to] += _value;
-
-        Transfer(_from, _to, _value);
-
-        assert(energyBalanceOf[_from] + energyBalanceOf[_to] == previousBalances);
     }
 
     /**
